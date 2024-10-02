ca496100a6bb17dd72bfabf8e0663c4084efeed4
sagar sharma <sagarsharma0028@gmail.com>
2024-10-02 16:46:35 +0530

    feat: add vendor and delivery app parent route file

--- apps/backend/source/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from "express";
-import userController from "../controllers/userController";
-
-const apiV1 = `/api/v1/`;
-
-
-export default (app: Express) => {
-    app.use(`${apiV1}user`, userController.router);
-};
